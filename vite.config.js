72469a2ee0f1fc7e55447c8a291890d23d479f66
Robinsonwells <17crusoe@gmail.com>
2025-12-30 12:46:56 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: false,
+    open: false,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': [
+            'react',
+            'react-dom',
+          ],
+          'transformers': [
+            '@xenova/transformers',
+          ],
+        },
+      },
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'idb',
+    ],
+  },
+})
