054c6c96b6453e24bd239e96714a524f72772550
Robinsonwells <17crusoe@gmail.com>
2025-12-31 11:29:47 -0700

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+/**
+ * PostCSS Configuration
+ * 
+ * Processes CSS with Tailwind CSS and Autoprefixer
+ * 
+ * Plugins:
+ * - tailwindcss: Generates utility classes
+ * - autoprefixer: Adds vendor prefixes for browser compatibility
+ */
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+}
